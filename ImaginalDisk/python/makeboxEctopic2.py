a6d378b504d198a7e38a01fb5b1cc66d672769eb
Matheus Viana <vianamp@users.noreply.github.com>
2017-11-25 13:06:02 -0200

    Update makeboxEctopic2.py

--- ImaginalDisk/python/makeboxEctopic2.py
+++ ImaginalDisk/python/makeboxEctopic2.py
@@ -1,20 +1,30 @@
-RootFolder = '/Volumes/WAILERS/UCI/Collaborators/Marcos/Data/EctopicExperiment_new/Temp/29C39H/Experimental'
+#!/usr/bin/env python
+import os
+import vtk
+import math
+import numpy
+from paraview.simple import *
+import paraview.servermanager
 
-File = 'CA_8_39h29C_Exp_B8'
+#RootFolder = '/Volumes/WAILERS/UCI/Collaborators/Marcos/Data/NoFoldExperiment-TimeVaryingData/18C/Control'
+
+#File = 'CA_9_18C_Ctrl_A1'
+
+FilePath = raw_input("prompt")
 
 S = ExtractSurface(Input=GetActiveSource())
 
 T = Triangulate(Input=S)
 
 conn = vtk.vtkPolyDataConnectivityFilter()
-conn.SetInputData(servermanager.Fetch(S))
-conn.SetExtractionMode(3)
-conn.AddSpecifiedRegion(0)
+conn.SetInputData(servermanager.Fetch(T))
+conn.SetExtractionModeToSpecifiedRegions()
+conn.AddSpecifiedRegion(1)
 conn.Update()
 
 S = conn.GetOutput()
 
 writer = vtk.vtkPolyDataWriter()
-writer.SetFileName(os.path.join(RootFolder,File+'-DiskDec.vtk'))
+writer.SetFileName(FilePath+'-DiskDec.vtk')
 writer.SetInputData(S)
 writer.Write()
